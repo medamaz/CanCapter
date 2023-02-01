ebd3df0687375518e363d980864feb85836db47e
moami <mo.amine.az@gmail.com>
2023-02-01 04:56:19 +0100

    fbfdb

--- CanCapter/Program.cs
+++ CanCapter/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +17,17 @@ namespace CanCapter
         [STAThread]
         static void Main()
         {
+            try
+            {
+                FileIOPermission f = new FileIOPermission(FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, Directory.GetCurrentDirectory());
+                f.Demand();
+
+                // Your code that accesses the system folder goes here
+            }
+            catch (SecurityException se)
+            {
+                Console.WriteLine("Permission to access the system folder was not granted: " + se.Message);
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Accueil());
